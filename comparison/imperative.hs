739674d990507e27b90bdb6c31e67dbbd2bf04cf
Ryan Newton <rrnewton@gmail.com>
2014-11-28 15:17:31 -0500

    Improve the imperative version to use bytestring and now its as fast as the OCaml one

--- comparison/imperative.hs
+++ comparison/imperative.hs
@@ -1,15 +1,22 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, OverloadedStrings #-}
 import System.Environment
-import System.FilePath
-import System.Directory
-import System.Posix.Files
-import System.Posix.Directory
+-- import System.FilePath
+-- import System.Directory
+import System.Posix.Directory.ByteString
+import System.Posix.Files.ByteString -- (getSymbolicLinkStatus)
+import System.Posix.Directory.ByteString
+import qualified Data.ByteString.Char8 as B
 import Data.Int
+import Data.Word
 
 main = do [dir] <- getArgs
-	  sum <- traverse dir
+	  sum <- traverse (B.pack dir)
 	  putStrLn$ "Total bytes: "++ show sum
 
+type Scalar = Word64
+-- type Scalar = COff
+
+traverse :: B.ByteString -> IO Scalar
 traverse path = do
   de   <- openDirStream path
   strm <- readDirStream de 
@@ -18,11 +25,13 @@ traverse path = do
   return sum
  where
     -- Empty string signals end of stream:
+    go :: DirStream -> Scalar -> B.ByteString -> IO Scalar
     go d !sum ""   = return sum
     go d !sum "."  = readDirStream d >>= go d sum
     go d !sum ".." = readDirStream d >>= go d sum
     go d !sum  x   = 
-       do let newpath = path </> x
+--       do let newpath = path </> x
+       do let newpath = path `B.append` "/" `B.append` x      
 	  stat <- getSymbolicLinkStatus newpath 
 	  if isDirectory stat 
 	   then do !n   <- traverse newpath 
@@ -30,7 +39,7 @@ traverse path = do
 		   go d (n + sum) strm
 	   else do 
 		   let newsum = if isRegularFile stat 
-				then sum + (fileSize stat)
+				then sum + fromIntegral (fileSize stat)
 				else sum
 		   next <- readDirStream d 
 		   go d newsum next		 
