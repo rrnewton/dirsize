66c592c40f81f612eaea9695e05373d2e4fb36d7
Ryan Newton <rrnewton@gmail.com>
2017-10-04 10:34:51 -0400

    Package for Nix.  Tested with 17.03.1904.09b9f7e7c4 (Gorilla) as both user nix-env plus underlying nixos version.

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> {}
+#, stdenv
+#, ocaml
+}:
+
+with pkgs;
+
+stdenv.mkDerivation rec {
+  name = "dirsize-${version}";
+  version = "0.9";
+
+  buildInputs = [ ocaml ];
+
+  src = builtins.filterSource
+    (path: type: type != "directory" ||
+    (baseNameOf path != ".git" && baseNameOf path != "haskell_ver"))
+      ./. ;
+
+  # This is hacky because the Makefile builds in-place (mutates src dir):
+  builder = builtins.toFile "builder.sh" "
+    source $stdenv/setup
+    cp -a $src/ ./src
+    chown -R `whoami` ./src
+    chmod u+rwX -R ./src
+    cd ./src
+    make
+    mkdir -p $out/bin
+    cp dirsize mods $out/bin/
+  ";
+
+}
